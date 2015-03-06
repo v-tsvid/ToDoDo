bd36e40bc8c471d0cab16ed8b8867e21ce2f19f2
Vadim Tsvid <v.tsvid@i.ua>
2015-03-06 19:42:59 +0200

    Add helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,12 +8,17 @@ describe "StaticPages" do
     it "should have the content 'ToDoDoo'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      expect(page).to have_content('ToDoDoo')
+      expect(page).to have_content('ToDoDooApp')
     end
 
     it "should have the right title" do
   	  visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("ToDoDoo")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title("| Home")
     end
   end
 
