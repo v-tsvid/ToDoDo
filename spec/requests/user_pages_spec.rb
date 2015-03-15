4ec969e16ab5720e8aa9b36dc75c0367dc612fef
Vadim Tsvid <v.tsvid@i.ua>
2015-03-15 13:03:41 +0200

    Finish sign-in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -38,6 +38,103 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
+
+    describe "after submission" do
+
+      describe "without information" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+
+      describe "with blank name" do
+        before do 
+          fill_in "Name",         with: ""
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content("Name can't be blank") }
+      end      
+
+      describe "with blank email" do
+        before do 
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: ""
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content("Email can't be blank") }
+      end      
+
+      describe "with blank password" do
+        before do 
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: ""
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content("Password can't be blank") }
+      end      
+
+      describe "with invalid email" do
+        before do 
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "userexamplecom"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content("Email is invalid") }
+      end      
+
+      describe "with too short password" do
+        before do 
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "fooba"
+          fill_in "Confirmation", with: "fooba"
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content("Password is too short") }
+      end   
+
+      describe "with confirmation not match password" do
+        before do 
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "user@example.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobat"
+          click_button submit
+        end
+
+        it { should have_title('Sign up') }
+        it { should have_content("confirmation doesn't match") }
+      end         
     end
   end
 end
